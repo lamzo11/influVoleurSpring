d14db8b49700e1409dc70c2d47298249247f0203
Lamine Diaw <laminediaw111@gmail.com>
2025-05-20 18:04:49 +0200

    beginning of an influvoleur story

---
+++ src/main/java/com/examplelamzomosh/influvoleurposey/InfluvoleurposeyApplication.java
@@ -0,0 +1,13 @@
+package com.examplelamzomosh.influvoleurposey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluvoleurposeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluvoleurposeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplelamzomosh/influvoleurposey/InfluvoleurposeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplelamzomosh.influvoleurposey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluvoleurposeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
