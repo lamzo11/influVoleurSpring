26f027b564057e56b4f9ff3bb3e94cfaf68a7748
Lamine Diaw <laminediaw111@gmail.com>
2025-05-20 19:49:43 +0200

    Added webdev dependencies and tested html file creation

---
+++ src/main/java/com/examplelamzomosh/influvoleurposey/HomeController.java
@@ -0,0 +1,13 @@
+package com.examplelamzomosh.influvoleurposey;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
--- src/test/java/com/examplelamzomosh/influvoleurposey/InfluvoleurposeyApplicationTests.java
+++ src/test/java/com/examplelamzomosh/influvoleurposey/InfluvoleurposeyApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InfluvoleurposeyApplicationTests {
-	//
+
 
 	@Test
 	void contextLoads() {
